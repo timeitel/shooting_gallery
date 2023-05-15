e86ef424f48a9eff1660225eb568baf21336f2ec
Tim Eitel <timeitel@outlook.com>
2023-05-16 08:50:32 +1000

    feat: a cube!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+use bevy_editor_pls::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EditorPlugin::default())
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    // let handle = asset_server.load("assets/sprites/background_003.png");
+    // let mut texture_atlas = TextureAtlas::new_empty(handle, Vec2::new(794.0, 802.0));
+    // let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("sprites/background_0003.png"),
+        ..default()
+    });
 }
