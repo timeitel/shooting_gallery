63062653d7624a1a6450c7082a5117e7bdd68fab
Tim Eitel <timeitel@outlook.com>
2023-05-16 20:50:28 +1000

    feat: asset spawning

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 use bevy::prelude::*;
 use bevy_editor_pls::prelude::*;
 
+#[derive(Component)]
+struct MainCamera;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EditorPlugin::default())
         .add_startup_system(setup)
+        .add_system(spawn_ball)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    // let handle = asset_server.load("assets/sprites/background_003.png");
-    // let mut texture_atlas = TextureAtlas::new_empty(handle, Vec2::new(794.0, 802.0));
-    // let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    commands.spawn(Camera2dBundle::default());
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((Camera2dBundle::default(), MainCamera));
     commands.spawn(SpriteBundle {
         texture: asset_server.load("sprites/background_0003.png"),
         ..default()
     });
+}
+
+fn spawn_ball(mut commands: Commands, input: Res<Input<KeyCode>>, asset_server: Res<AssetServer>) {
+    if input.just_pressed(KeyCode::Space) {
+        commands.spawn(SpriteBundle {
+            texture: asset_server.load("sprites/background_0003.png"),
+            ..default()
+        });
+    }
 }
